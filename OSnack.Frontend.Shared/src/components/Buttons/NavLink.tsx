96f6217e2e44144ac98546ac472f4fbb2e4693c7
Majid Joveini <majid.joveini@gmail.com>
2020-11-26 14:02:47 +0000

    revert webpack changes back to working version

--- OSnack.Frontend.Shared/src/components/Buttons/NavLink.tsx
+++ OSnack.Frontend.Shared/src/components/Buttons/NavLink.tsx
@@ -1,5 +1,4 @@
-﻿
-import React, { useState } from 'react';
+﻿import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { setHtmlTitle } from '../../_core/appFunc';
 const NavLink = (props: IProps) => {
